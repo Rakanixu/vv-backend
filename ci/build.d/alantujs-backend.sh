83bcce259a94a5c63657c64a38cb3f25e9214d5a
J. Oliva <jesus@epiclabs.io>
2017-07-05 14:36:41 +0200

    Fix bug in alantujs-backend ci script

--- ci/build.d/alantujs-backend.sh
+++ ci/build.d/alantujs-backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! docker build -t "$PREFIX/admin:$TAG" . ; then
-	echo "Error building image"
-	exit 1;
+if ! docker build -t "$PREFIX/alantujs-backend:$TAG" . ; then
+    echo "Error building image"
+    exit 1;
 fi
