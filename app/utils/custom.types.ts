4257105c78c790a9b61533961b7507e745f3881a
Pablo Aguirre <paguirrerubio@gmail.com>
2017-07-06 19:39:08 +0200

    Resolve conflicts from merge origin/dev

--- app/utils/custom.types.ts
+++ app/utils/custom.types.ts
@@ -1,16 +1,10 @@
 import * as express from 'express';
 import { UserAccount } from '../api/user-account/user-account.model';
-<<<<<<< HEAD
 import { EventLocation } from '../api/event-location/event-location.model';
-
-export interface ICustomRequest extends express.Request {
-    user: UserAccount;
-    eventLocation: EventLocation;
-=======
 import { Principal } from '../api/principal/principal.model';
 
 export interface ICustomRequest extends express.Request {
     user: UserAccount;
     principal: Principal;
->>>>>>> origin/dev
+    eventLocation: EventLocation;
 }
