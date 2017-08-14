1445dd86081e6b2c6c095aabe2795b04a219e001
Pablo Aguirre <paguirrerubio@gmail.com>
2017-08-14 14:38:34 +0200

    Debug

--- app/server.ts
+++ app/server.ts
@@ -33,14 +33,14 @@ export class Server {
         this.app.use(cookieParser());
         this.app.use(methodOverride());
         this.app.use(express.static(__dirname + '/static'));
-        this.app.use((req: ICustomRequest, res: express.Response, next: Function) => {
+        /* this.app.use((req: ICustomRequest, res: express.Response, next: Function) => {
             // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
             res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
             res.header('Access-Control-Allow-Credentials', 'true');
             res.header('Access-Control-Allow-Headers', 'Origin, Credentials, X-Requested-With, Content-Type, Accept, Authorization');
             console.log('SET HEADERS');
             next();
-        });
+        }); */
 
         const env = process.env.NODE_ENV || 'development';
         if (env === 'development') {
